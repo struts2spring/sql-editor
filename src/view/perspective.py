cb9118992a8c0f988655296f9c7eb750c04dc7a9
Vijay <>
2018-11-28 16:44:23 +0530

    saving changes: perspective

---
+++ src/view/perspective.py
@@ -0,0 +1,237 @@
+import wx
+
+import logging.config
+from src.view.constants import LOG_SETTINGS, ID_newConnection, ID_openConnection,\
+    ID_newWorksheet
+from src.sqlite_executer.ConnectExecuteSqlite import SQLExecuter
+from src.view.AutoCompleteTextCtrl import TextCtrlAutoComplete
+from src.view.TreePanel import CreatingTreePanel
+from src.view.worksheet.WorksheetPanel import CreateWorksheetTabPanel
+from src.view.SqlOutputPanel import SqlConsoleOutputPanel
+from src.view.history.HistoryListPanel import HistoryGrid
+
+logging.config.dictConfig(LOG_SETTINGS)
+logger = logging.getLogger('extensive')
+
+
+try:
+    from agw import aui
+    from agw.aui import aui_switcherdialog as ASD
+except ImportError:  # if it's not there locally, try the wxPython lib.
+    import wx.lib.agw.aui as aui
+    from wx.lib.agw.aui import aui_switcherdialog as ASD
+############################################################
+
+
+class PerspectiveManager(object):
+    """Creates a perspective manager for the given aui managed window.
+    It supports saving and loading of on disk perspectives as created by
+    calling SavePerspective from the AuiManager. Mixin class for a wx.Frame.
+
+    """
+    def __init__(self, base=None):
+        """Initializes the perspective manager. The auimgr parameter is
+        a reference to the windows AuiManager instance, base is the base
+        path to where perspectives should be loaded from and saved to.
+        @param base: path to configuration cache
+
+        """
+        super(PerspectiveManager, self).__init__()
+
+        self.createAuiManager()
+        
+    def createAuiManager(self):
+        logger.debug('createAuiManager')
+        # tell FrameManager to manage this frame
+        self._mgr = aui.AuiManager()
+        self._mgr.SetManagedWindow(self)
+        # set up default notebook style
+        self._notebook_style = aui.AUI_NB_DEFAULT_STYLE | aui.AUI_NB_TAB_EXTERNAL_MOVE | wx.NO_BORDER
+        self._notebook_theme = 0      
+        # min size for the frame itself isn't completely done.
+        # see the end up AuiManager.Update() for the test
+        # code. For now, just hard code a frame minimum size
+        self.SetMinSize(wx.Size(400, 300))    
+        self._perspectives = []
+        
+        # add a bunch of panes
+#         self._mgr.AddPane(self.CreateSizeReportCtrl(), wx.aui.AuiPaneInfo().Name("test1").Caption("Pane Caption").Top().CloseButton(True).MaximizeButton(True))
+                # add the toolbars to the manager
+
+        self._mgr.AddPane(self.constructToolBar(), aui.AuiPaneInfo().
+                          Name("tb1").Caption("Big Toolbar").
+                          ToolbarPane().Top().CloseButton(True).
+                          LeftDockable(False).RightDockable(False).Gripper(True))    
+        
+        self._mgr.AddPane(self.creatingTreeCtrl(), aui.AuiPaneInfo().Icon(self.fileOperations.getImageBitmap(imageName="folder_database.png")).
+                          Name("databaseNaviagor").Caption("Database Navigator").Dockable(True).Movable(True).MinSize(wx.Size(300, 100)).
+                          Left().Layer(1).Position(1).CloseButton(False).MaximizeButton(True).MinimizeButton(True))
+     
+        self._mgr.AddPane(self.constructSqlPane(), aui.AuiPaneInfo().Icon(self.fileOperations.getImageBitmap(imageName="script.png")).
+                          Name("sqlExecution").Caption("SQL execution").LeftDockable(True).
+                          Center().CloseButton(True).MaximizeButton(True).MinimizeButton(True))
+        
+#         self._mgr.AddPane(self.constructSchemaViewerPane(), aui.AuiPaneInfo().Icon(wx.Bitmap(os.path.join(path, "script.png"))).
+#                           Name("schemaViewer").Caption("Schema Viewer").LeftDockable(True).
+#                           Center().CloseButton(True).MaximizeButton(True).MinimizeButton(True))      
+#         self._mgr.AddPane(self.constructSchemaViewerPane(), aui.AuiPaneInfo().
+#                           Name("test9").Caption("Min Size 200x100").
+#                           BestSize(wx.Size(200, 100)).MinSize(wx.Size(200, 100)).
+#                           Bottom().Layer(1).CloseButton(True).MaximizeButton(True))      
+  
+        self._mgr.AddPane(self.sqlConsoleOutputPane(), aui.AuiPaneInfo().Icon(self.fileOperations.getImageBitmap(imageName="console_view.png")).
+                          Name("consoleOutput").Caption("Console").Dockable(True).Movable(True).LeftDockable(True).
+                          Bottom().Layer(0).Row(1).CloseButton(True).MaximizeButton(visible=True).MinimizeButton(visible=True).PinButton(visible=True).GripperTop())
+            
+        self._mgr.AddPane(self.constructHistoryPane(), aui.AuiPaneInfo().Icon(self.fileOperations.getImageBitmap(imageName="sql.png")).
+                          Name("sqlLog").Caption("SQL Log").Dockable(True).BestSize(wx.Size(200, 200)).
+                          Bottom().Layer(0).Row(1).CloseButton(True).MaximizeButton(visible=True).MinimizeButton(visible=True))
+            
+        self._mgr.GetPane("tb1").Show()
+        self.perspective_default = self._mgr.SavePerspective()
+        perspective_all = self._mgr.SavePerspective()
+        all_panes = self._mgr.GetAllPanes()
+        # "commit" all changes made to FrameManager
+        self._mgr.Update()  
+        
+    def constructToolBar(self):
+        # create some toolbars
+        tb1 = aui.AuiToolBar(self, -1, wx.DefaultPosition, wx.DefaultSize, agwStyle=aui.AUI_TB_DEFAULT_STYLE | aui.AUI_TB_OVERFLOW)
+        
+        tb1.SetToolBitmapSize(wx.Size(42, 42))
+        tb1.AddSimpleTool(tool_id=ID_newConnection, label="New Connection", bitmap=wx.Bitmap(self.fileOperations.getImageBitmap(imageName="connect.png")), short_help_string='Create a new connection')
+        tb1.AddSeparator()
+        
+        tb1.AddSimpleTool(ID_openConnection, "Open Connection", wx.Bitmap(self.fileOperations.getImageBitmap(imageName="database_connect.png")), short_help_string='Open Connection')
+        tb1.AddSimpleTool(ID_newWorksheet, "Script", wx.Bitmap(self.fileOperations.getImageBitmap(imageName="script.png")), short_help_string='Open a new script worksheet')
+        tb1.AddSimpleTool(wx.ID_PREFERENCES, "Preferences", wx.Bitmap(self.fileOperations.getImageBitmap(imageName="preference.png")), short_help_string='Preference')
+#         tb1.DoGetBestSize()
+        ###################################################################################################
+        args = {}
+        if True:
+            args["colNames"] = ("col1", "col2")
+            args["multiChoices"] = [ ("Zoey", "WOW"), ("Alpha", "wxPython"),
+                                    ("Ceda", "Is"), ("Beta", "fantastic"),
+                                    ("zoebob", "!!")]
+            args["colFetch"] = 1
+        else:
+            args["choices"] = ["123", "cs", "cds", "Bob", "Marley", "Alpha"]
+        args["selectCallback"] = self.selectCallback   
+        self.dynamic_choices = list()
+        sqlExecuter = SQLExecuter()
+        dbList = sqlExecuter.getListDatabase()  
+        for db in dbList:
+            self.dynamic_choices.append(db[1])
+           
+#         self.dynamic_choices = [
+#                 'aardvark', 'abandon', 'acorn', 'acute', 'adore',
+#                 'aegis', 'ascertain', 'asteroid',
+#                 'beautiful', 'bold', 'classic',
+#                 'daring', 'dazzling', 'debonair', 'definitive',
+#                 'effective', 'elegant',
+#                 'http://python.org', 'http://www.google.com',
+#                 'fabulous', 'fantastic', 'friendly', 'forgiving', 'feature',
+#                 'sage', 'scarlet', 'scenic', 'seaside', 'showpiece', 'spiffy',
+#                 'www.wxPython.org', 'www.osafoundation.org'
+#                 ]
+
+        self._ctrl = TextCtrlAutoComplete(tb1, **args)
+        self._ctrl.SetSize((250, 25))
+        self._ctrl.SetChoices(self.dynamic_choices)
+        self._ctrl.SetEntryCallback(self.setDynamicChoices)
+        self._ctrl.SetMatchFunction(self.match)
+        tb1.AddControl(self._ctrl) 
+
+        ###################################################################################################
+#         tb1.AddControl( self.choice ) 
+#         tb1.AddLabelTool(103, "Test", wx.ArtProvider_GetBitmap(wx.ART_INFORMATION))
+#         tb1.AddLabelTool(103, "Test"t1 = wx.TextCtrl(self, -1, "Test it out and see", size=(125, -1)), wx.ArtProvider_GetBitmap(wx.ART_WARNING))
+#         tb1.AddLabelTool(103, "Test", wx.ArtProvider_GetBitmap(wx.ART_MISSING_IMAGE))
+        tb1.Realize()
+        
+        return tb1
+    
+    def creatingTreeCtrl(self):
+        # Create a TreeCtrl
+        treePanel = CreatingTreePanel(self)
+
+        return treePanel
+    
+    def constructSqlPane(self):
+        worksheet = CreateWorksheetTabPanel(self)      
+#         worksheet.addTab('Start Page')
+        return worksheet
+    
+    def sqlConsoleOutputPane(self):
+        sqlConsoleOutputPanel = SqlConsoleOutputPanel(self)
+        return sqlConsoleOutputPanel
+    
+    def constructHistoryPane(self):
+        historyGrid = HistoryGrid(self)
+        return historyGrid
+    
+    def CreateSizeReportCtrl(self, width=80, height=80):
+
+        ctrl = SizeReportCtrl(self, -1, wx.DefaultPosition,
+                              wx.Size(width, height), self._mgr)
+        return ctrl
+        
+        
+class SizeReportCtrl(wx.PyControl):
+
+    def __init__(self, parent, id=wx.ID_ANY, pos=wx.DefaultPosition,
+                 size=wx.DefaultSize, mgr=None):
+
+        wx.PyControl.__init__(self, parent, id, pos, size, wx.NO_BORDER)
+
+        self._mgr = mgr
+
+#         self.Bind(wx.EVT_PAINT, self.OnPaint)
+        self.Bind(wx.EVT_SIZE, self.OnSize)
+        self.Bind(wx.EVT_ERASE_BACKGROUND, self.OnEraseBackground)
+
+    def OnPaint(self, event):
+
+        dc = wx.PaintDC(self)
+
+        size = self.GetClientSize()
+        s = ("Size: %d x %d") % (size.x, size.y)
+
+        dc.SetFont(wx.NORMAL_FONT)
+        w, height = dc.GetTextExtent(s)
+        height = height + 3
+        dc.SetBrush(wx.WHITE_BRUSH)
+        dc.SetPen(wx.WHITE_PEN)
+        dc.DrawRectangle(0, 0, size.x, size.y)
+        dc.SetPen(wx.LIGHT_GREY_PEN)
+        dc.DrawLine(0, 0, size.x, size.y)
+        dc.DrawLine(0, size.y, size.x, 0)
+        dc.DrawText(s, (size.x - w) / 2, ((size.y - (height * 5)) / 2))
+
+        if self._mgr:
+
+            pi = self._mgr.GetPane(self)
+
+            s = ("Layer: %d") % pi.dock_layer
+            w, h = dc.GetTextExtent(s)
+            dc.DrawText(s, (size.x - w) / 2, ((size.y - (height * 5)) / 2) + (height * 1))
+
+            s = ("Dock: %d Row: %d") % (pi.dock_direction, pi.dock_row)
+            w, h = dc.GetTextExtent(s)
+            dc.DrawText(s, (size.x - w) / 2, ((size.y - (height * 5)) / 2) + (height * 2))
+
+            s = ("Position: %d") % pi.dock_pos
+            w, h = dc.GetTextExtent(s)
+            dc.DrawText(s, (size.x - w) / 2, ((size.y - (height * 5)) / 2) + (height * 3))
+
+            s = ("Proportion: %d") % pi.dock_proportion
+            w, h = dc.GetTextExtent(s)
+            dc.DrawText(s, (size.x - w) / 2, ((size.y - (height * 5)) / 2) + (height * 4))
+
+    def OnEraseBackground(self, event):
+        # intentionally empty
+        pass
+
+    def OnSize(self, event):
+        self.Refresh()
+        event.Skip()
--- src/view/sql_editor_view.py
+++ src/view/sql_editor_view.py
@@ -1,40 +1,29 @@
 import logging
-import os
 import platform
 import sys
 
 from wx import  ID_PREFERENCES
 import wx
 
-from src.sqlite_executer.ConnectExecuteSqlite import SQLExecuter
-from src.view.AutoCompleteTextCtrl import TextCtrlAutoComplete
-from src.view.SqlOutputPanel import SqlConsoleOutputPanel
-from src.view.TreePanel import CreatingTreePanel
+
 from src.view.connection.NewConnectionWizard import CreateNewConncetionWixard
 from src.view.constants import ID_openConnection, ID_newWorksheet, ID_newConnection, \
     ID_SQL_EXECUTION, ID_SQL_LOG, ID_UPDATE_CHECK, TITLE, VERSION, \
     ID_HIDE_TOOLBAR, ID_APPEARANCE, ID_SEARCH_FILE, ID_CONSOLE_LOG, ID_SHOW_VIEW, \
     ID_PROSPECTIVE_NAVIGATION
-from src.view.history.HistoryListPanel import HistoryGrid
+
 from src.view.openConnection.OpenExistingConnection import OpenExistingConnectionFrame
 from src.view.preference.OpalPreferences import OpalPreference
 from src.view.util.FileOperationsUtil import FileOperations
-from src.view.worksheet.WorksheetPanel import CreateWorksheetTabPanel
+
+from src.view.perspective import PerspectiveManager
 
 
-# from src.view.AutoCompleteTextCtrl import TextCtrlAutoComplete
-# from src.view.openConnection.OpenExistingConnection import OpenExistingConnectionFrame
-try:
-    from agw import aui
-    from agw.aui import aui_switcherdialog as ASD
-except ImportError:  # if it's not there locally, try the wxPython lib.
-    import wx.lib.agw.aui as aui
-    from wx.lib.agw.aui import aui_switcherdialog as ASD
     
 logger = logging.getLogger('extensive')
 
 
-class DatabaseMainFrame(wx.Frame):
+class DatabaseMainFrame(wx.Frame, PerspectiveManager):
 
     def __init__(self, parent):
         logger.info("This is from Runner ")
@@ -51,78 +40,21 @@ class DatabaseMainFrame(wx.Frame):
         self.SetMinSize(wx.Size(400, 300))
         self.createMenuBar()
         self.createStatusBar()
+        
 #         self.creatingTreeCtrl()
         
         try:
-            self.createAuiManager()
+            PerspectiveManager.__init__(self)
         except Exception as e:
             logger.error(e, exc_info=True)
+            
         self.bindingEvent()
         self._mgr.Update()  
 
-    def creatingTreeCtrl(self):
-        # Create a TreeCtrl
-        treePanel = CreatingTreePanel(self)
 
-        return treePanel
     #---------------------------------------------    
          
-    def constructToolBar(self):
-        # create some toolbars
-        tb1 = aui.AuiToolBar(self, -1, wx.DefaultPosition, wx.DefaultSize, agwStyle=aui.AUI_TB_DEFAULT_STYLE | aui.AUI_TB_OVERFLOW)
-        
-        tb1.SetToolBitmapSize(wx.Size(42, 42))
-        tb1.AddSimpleTool(tool_id=ID_newConnection, label="New Connection", bitmap=wx.Bitmap(self.fileOperations.getImageBitmap(imageName="connect.png")), short_help_string='Create a new connection')
-        tb1.AddSeparator()
-        
-        tb1.AddSimpleTool(ID_openConnection, "Open Connection", wx.Bitmap(self.fileOperations.getImageBitmap(imageName="database_connect.png")), short_help_string='Open Connection')
-        tb1.AddSimpleTool(ID_newWorksheet, "Script", wx.Bitmap(self.fileOperations.getImageBitmap(imageName="script.png")), short_help_string='Open a new script worksheet')
-        tb1.AddSimpleTool(wx.ID_PREFERENCES, "Preferences", wx.Bitmap(self.fileOperations.getImageBitmap(imageName="preference.png")), short_help_string='Preference')
-#         tb1.DoGetBestSize()
-        ###################################################################################################
-        args = {}
-        if True:
-            args["colNames"] = ("col1", "col2")
-            args["multiChoices"] = [ ("Zoey", "WOW"), ("Alpha", "wxPython"),
-                                    ("Ceda", "Is"), ("Beta", "fantastic"),
-                                    ("zoebob", "!!")]
-            args["colFetch"] = 1
-        else:
-            args["choices"] = ["123", "cs", "cds", "Bob", "Marley", "Alpha"]
-        args["selectCallback"] = self.selectCallback   
-        self.dynamic_choices = list()
-        sqlExecuter = SQLExecuter()
-        dbList = sqlExecuter.getListDatabase()  
-        for db in dbList:
-            self.dynamic_choices.append(db[1])
-           
-#         self.dynamic_choices = [
-#                 'aardvark', 'abandon', 'acorn', 'acute', 'adore',
-#                 'aegis', 'ascertain', 'asteroid',
-#                 'beautiful', 'bold', 'classic',
-#                 'daring', 'dazzling', 'debonair', 'definitive',
-#                 'effective', 'elegant',
-#                 'http://python.org', 'http://www.google.com',
-#                 'fabulous', 'fantastic', 'friendly', 'forgiving', 'feature',
-#                 'sage', 'scarlet', 'scenic', 'seaside', 'showpiece', 'spiffy',
-#                 'www.wxPython.org', 'www.osafoundation.org'
-#                 ]
 
-        self._ctrl = TextCtrlAutoComplete(tb1, **args)
-        self._ctrl.SetSize((250, 25))
-        self._ctrl.SetChoices(self.dynamic_choices)
-        self._ctrl.SetEntryCallback(self.setDynamicChoices)
-        self._ctrl.SetMatchFunction(self.match)
-        tb1.AddControl(self._ctrl) 
-
-        ###################################################################################################
-#         tb1.AddControl( self.choice ) 
-#         tb1.AddLabelTool(103, "Test", wx.ArtProvider_GetBitmap(wx.ART_INFORMATION))
-#         tb1.AddLabelTool(103, "Test"t1 = wx.TextCtrl(self, -1, "Test it out and see", size=(125, -1)), wx.ArtProvider_GetBitmap(wx.ART_WARNING))
-#         tb1.AddLabelTool(103, "Test", wx.ArtProvider_GetBitmap(wx.ART_MISSING_IMAGE))
-        tb1.Realize()
-        
-        return tb1
 
     def selectCallback(self, values):
         """ Simply function that receive the row values when the
@@ -151,75 +83,16 @@ class DatabaseMainFrame(wx.Frame):
         if c.startswith('www.'): c = c[4:]
         return c.startswith(t)    
 
-    def createAuiManager(self):
-        logger.debug('createAuiManager')
-        # tell FrameManager to manage this frame
-        self._mgr = aui.AuiManager()
-        self._mgr.SetManagedWindow(self)
-        # set up default notebook style
-        self._notebook_style = aui.AUI_NB_DEFAULT_STYLE | aui.AUI_NB_TAB_EXTERNAL_MOVE | wx.NO_BORDER
-        self._notebook_theme = 0      
-        # min size for the frame itself isn't completely done.
-        # see the end up AuiManager.Update() for the test
-        # code. For now, just hard code a frame minimum size
-        self.SetMinSize(wx.Size(400, 300))    
-        self._perspectives = []
-        
-        # add a bunch of panes
-#         self._mgr.AddPane(self.CreateSizeReportCtrl(), wx.aui.AuiPaneInfo().Name("test1").Caption("Pane Caption").Top().CloseButton(True).MaximizeButton(True))
-                # add the toolbars to the manager
-
-        self._mgr.AddPane(self.constructToolBar(), aui.AuiPaneInfo().
-                          Name("tb1").Caption("Big Toolbar").
-                          ToolbarPane().Top().CloseButton(True).
-                          LeftDockable(False).RightDockable(False).Gripper(True))    
-        
-        self._mgr.AddPane(self.creatingTreeCtrl(), aui.AuiPaneInfo().Icon(self.fileOperations.getImageBitmap(imageName="folder_database.png")).
-                          Name("databaseNaviagor").Caption("Database Navigator").Dockable(True).Movable(True).MinSize(wx.Size(300, 100)).
-                          Left().Layer(1).Position(1).CloseButton(False).MaximizeButton(True).MinimizeButton(True))
      
-        self._mgr.AddPane(self.constructSqlPane(), aui.AuiPaneInfo().Icon(self.fileOperations.getImageBitmap(imageName="script.png")).
-                          Name("sqlExecution").Caption("SQL execution").LeftDockable(True).
-                          Center().CloseButton(True).MaximizeButton(True).MinimizeButton(True))
-        
-#         self._mgr.AddPane(self.constructSchemaViewerPane(), aui.AuiPaneInfo().Icon(wx.Bitmap(os.path.join(path, "script.png"))).
-#                           Name("schemaViewer").Caption("Schema Viewer").LeftDockable(True).
-#                           Center().CloseButton(True).MaximizeButton(True).MinimizeButton(True))      
-#         self._mgr.AddPane(self.constructSchemaViewerPane(), aui.AuiPaneInfo().
-#                           Name("test9").Caption("Min Size 200x100").
-#                           BestSize(wx.Size(200, 100)).MinSize(wx.Size(200, 100)).
-#                           Bottom().Layer(1).CloseButton(True).MaximizeButton(True))      
-  
-        self._mgr.AddPane(self.sqlConsoleOutputPane(), aui.AuiPaneInfo().Icon(self.fileOperations.getImageBitmap(imageName="console_view.png")).
-                          Name("consoleOutput").Caption("Console").Dockable(True).Movable(True).LeftDockable(True).
-                          Bottom().Layer(0).Row(1).CloseButton(True).MaximizeButton(visible=True).MinimizeButton(visible=True).PinButton(visible=True).GripperTop())
-            
-        self._mgr.AddPane(self.constructHistoryPane(), aui.AuiPaneInfo().Icon(self.fileOperations.getImageBitmap(imageName="sql.png")).
-                          Name("sqlLog").Caption("SQL Log").Dockable(True).BestSize(wx.Size(200, 200)).
-                          Bottom().Layer(0).Row(1).CloseButton(True).MaximizeButton(visible=True).MinimizeButton(visible=True))
-            
-        self._mgr.GetPane("tb1").Show()
-        self.perspective_default = self._mgr.SavePerspective()
-        perspective_all = self._mgr.SavePerspective()
-        all_panes = self._mgr.GetAllPanes()
-        # "commit" all changes made to FrameManager
-        self._mgr.Update()        
 
-    def constructHistoryPane(self):
-        historyGrid = HistoryGrid(self)
-        return historyGrid
+
     
 #     def constructSchemaViewerPane(self):
 #         svgViewer = SVGViewerPanel(self)
 #         return svgViewer
-    def sqlConsoleOutputPane(self):
-        sqlConsoleOutputPanel = SqlConsoleOutputPanel(self)
-        return sqlConsoleOutputPanel
 
-    def constructSqlPane(self):
-        worksheet = CreateWorksheetTabPanel(self)      
-#         worksheet.addTab('Start Page')
-        return worksheet
+
+
     
     def getCurrentCursorPosition(self):
         lineNo = 1
@@ -289,8 +162,8 @@ class DatabaseMainFrame(wx.Frame):
                                                 [ID_CONSOLE_LOG, 'Console', "console_view.png", None ]
                                             ], None
                     ],
-                    [ID_PROSPECTIVE_NAVIGATION, "Prospective", [
-                                                [ wx.NewIdRef(), 'Open Prospective', None, [
+                    [ID_PROSPECTIVE_NAVIGATION, "Perspective", [
+                                                [ wx.NewIdRef(), 'Open Perspective', None, [
                                                         [ wx.NewIdRef(), 'Python', "python_16x16.png", None],
                                                         [ wx.NewIdRef(), 'Java', "java_workingset_wiz.png", None],
                                                         [ wx.NewIdRef(), 'Java EE', "java_workingset_wiz.png", None],
@@ -461,71 +334,7 @@ Python :{}""".format(TITLE, VERSION, plate, sys.version)
         dlg.ShowModal()
         dlg.Destroy()
         
-    def CreateSizeReportCtrl(self, width=80, height=80):
 
-        ctrl = SizeReportCtrl(self, -1, wx.DefaultPosition,
-                              wx.Size(width, height), self._mgr)
-        return ctrl
-        
-        
-class SizeReportCtrl(wx.PyControl):
-
-    def __init__(self, parent, id=wx.ID_ANY, pos=wx.DefaultPosition,
-                 size=wx.DefaultSize, mgr=None):
-
-        wx.PyControl.__init__(self, parent, id, pos, size, wx.NO_BORDER)
-
-        self._mgr = mgr
-
-#         self.Bind(wx.EVT_PAINT, self.OnPaint)
-        self.Bind(wx.EVT_SIZE, self.OnSize)
-        self.Bind(wx.EVT_ERASE_BACKGROUND, self.OnEraseBackground)
-
-    def OnPaint(self, event):
-
-        dc = wx.PaintDC(self)
-
-        size = self.GetClientSize()
-        s = ("Size: %d x %d") % (size.x, size.y)
-
-        dc.SetFont(wx.NORMAL_FONT)
-        w, height = dc.GetTextExtent(s)
-        height = height + 3
-        dc.SetBrush(wx.WHITE_BRUSH)
-        dc.SetPen(wx.WHITE_PEN)
-        dc.DrawRectangle(0, 0, size.x, size.y)
-        dc.SetPen(wx.LIGHT_GREY_PEN)
-        dc.DrawLine(0, 0, size.x, size.y)
-        dc.DrawLine(0, size.y, size.x, 0)
-        dc.DrawText(s, (size.x - w) / 2, ((size.y - (height * 5)) / 2))
-
-        if self._mgr:
-
-            pi = self._mgr.GetPane(self)
-
-            s = ("Layer: %d") % pi.dock_layer
-            w, h = dc.GetTextExtent(s)
-            dc.DrawText(s, (size.x - w) / 2, ((size.y - (height * 5)) / 2) + (height * 1))
-
-            s = ("Dock: %d Row: %d") % (pi.dock_direction, pi.dock_row)
-            w, h = dc.GetTextExtent(s)
-            dc.DrawText(s, (size.x - w) / 2, ((size.y - (height * 5)) / 2) + (height * 2))
-
-            s = ("Position: %d") % pi.dock_pos
-            w, h = dc.GetTextExtent(s)
-            dc.DrawText(s, (size.x - w) / 2, ((size.y - (height * 5)) / 2) + (height * 3))
-
-            s = ("Proportion: %d") % pi.dock_proportion
-            w, h = dc.GetTextExtent(s)
-            dc.DrawText(s, (size.x - w) / 2, ((size.y - (height * 5)) / 2) + (height * 4))
-
-    def OnEraseBackground(self, event):
-        # intentionally empty
-        pass
-
-    def OnSize(self, event):
-        self.Refresh()
-        event.Skip()
 
 
 if __name__ == "__main__":
