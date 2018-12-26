24f0bdd88bc495b6745517640f79dcbd8e504ad9
Vijay <>
2018-12-26 21:27:40 +0530

    save

---
+++ src/view/view/project/explorer/ProjectExplorer.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+'''
+Created on 13-Dec-2016
+
+@author: vijay
+'''
+import logging
+
+from src.view.util.FileOperationsUtil import FileOperations
+import wx
+
+
+# from src.view.table.CreateTable import CreateTableFrame
+logger = logging.getLogger('extensive')
+
+
+class CreatingProjectExplorerPanel(wx.Panel):
+
+    def __init__(self, parent=None, *args, **kw):
+        wx.Panel.__init__(self, parent, id=-1)
+        self.parent = parent
+        self.fileOperations = FileOperations()
+        vBox = wx.BoxSizer(wx.VERTICAL)
+        ####################################################################
+        ####################################################################
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(vBox, 1, wx.EXPAND , 0)
+        self.SetSizer(sizer)
+
+#---------------------------------------------------------------------------
+if __name__ == '__main__':
+#     treeImageLevel = dict()
+#     treeImageLevel[(0, 0)] = (0, 'database.png')
+#     treeImageLevel[(1, 0)] = (0, 'database_category.png')
+#     treeImageLevel[(1, 1)] = (0, 'folder_view.png')
+#     treeImageLevel[(1, 2)] = (0, 'folder.png')
+#     
+#     print(treeImageLevel[(0, 0)])
+    app = wx.App(False)
+    frame = wx.Frame(None)
+    try: 
+        panel = CreatingProjectExplorerPanel(frame, title='asfd')
+    except Exception as ex:
+        logger.error(ex)
+    frame.Show()
+    app.MainLoop()
