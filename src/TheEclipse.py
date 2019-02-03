0cd1fb715afda09b22a5ae3f14f60c14dd13c38e
vijay <struts2spring@gmail.com>
2019-02-03 18:19:49 +0530

    save

--- src/TheEclipse.py
+++ src/TheEclipse.py
@@ -3,20 +3,16 @@ import logging.config
 
 from src.view.constants import LOG_SETTINGS
 from src.view.EditorSplash import MyApp
-from http.server import HTTPServer, BaseHTTPRequestHandler
-from multiprocessing import Process
-from threading import Thread
-
 
 logger = logging.getLogger('extensive')
 
 logging.config.dictConfig(LOG_SETTINGS)
 
 
-
 def main():
     app = MyApp(False)
     app.MainLoop()
+
 
 if __name__ == '__main__':
     main()
