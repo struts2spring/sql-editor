bf3c9774733268cffc125fd11c00cedd0df70e50
vijay <@>
2019-02-19 13:26:53 +0530

    working on search panel

--- src/view/search/SearchFrame.py
+++ src/view/search/SearchFrame.py
@@ -22,8 +22,8 @@ logger = logging.getLogger('extensive')
 class SearchPanelsFrame(wx.Frame):
 
     def __init__(self, parent, Id=wx.ID_ANY, Title="", pos=wx.DefaultPosition,
-             size=wx.DefaultSize, style=wx.DEFAULT_FRAME_STYLE | wx.SUNKEN_BORDER|wx.STAY_ON_TOP):
-
+             size=wx.DefaultSize, style=wx.DEFAULT_FRAME_STYLE | wx.SUNKEN_BORDER | wx.STAY_ON_TOP):
+        style = style & (~wx.MINIMIZE_BOX)
         wx.Frame.__init__(self, parent, Id, Title, pos, size, style)
         
         self.fileOperations = FileOperations()
@@ -34,7 +34,9 @@ class SearchPanelsFrame(wx.Frame):
         self._mgr.SetManagedWindow(self)
  
         # set frame icon
-#         self.SetIcon(self.fileOperations.getImageBitmap(imageName='eclipse16.png'))
+        icon = wx.Icon()
+        icon.CopyFromBitmap(self.fileOperations.getImageBitmap(imageName='eclipse16.png'))
+        self.SetIcon(icon)
  
         # set up default notebook style
 #         self._mgr._autoNBStyle = aui.AUI_NB_DEFAULT_STYLE | aui.AUI_NB_TAB_EXTERNAL_MOVE | wx.NO_BORDER
@@ -55,20 +57,31 @@ class SearchPanelsFrame(wx.Frame):
 
     def BuildPanes(self):
 #         self.SetMinSize(wx.Size(400, 300))
+        # this is to set tab on top
+        self._mgr.SetAutoNotebookStyle(aui.AUI_NB_DEFAULT_STYLE | wx.BORDER_NONE)
+        
+        
         self.buttonPanel = CreateButtonPanel(self)
         self.fileSearchPanel = CreateFileSearchPanel(self)
+        fileSearchPaneInfo= aui.AuiPaneInfo().Name("fileSearch").Icon(self.fileOperations.getImageBitmap(imageName="search_history.png")).Caption("File Search")\
+                        .Direction(wx.TOP).Row(0).Center().Layer(0).Position(0).Dockable(True)\
+                        .CaptionVisible(False, left=False).MinimizeButton(False).CloseButton(False)
+        self._mgr.AddPane(self.fileSearchPanel, fileSearchPaneInfo)
         
-        self._mgr.AddPane(self.fileSearchPanel, aui.AuiPaneInfo().
-                          Name("sql").Caption("SQL")
-                          .Layer(1).Center().CaptionVisible(False, left=True)
-                          .MinimizeButton(True).CloseButton(False))
+
+        self.fileSearchPanel1 = CreateFileSearchPanel(self)
+        self._mgr.AddPane(self.fileSearchPanel1, aui.AuiPaneInfo().Icon(self.fileOperations.getImageBitmap(imageName="search_history.png")).
+                          Name(f"taskSearch").Caption(f"Task Search").Dockable(False)
+                          .Center().Layer(0).Position(1).CaptionVisible(False, left=False)
+                          .MinimizeButton(False).CloseButton(False),target=fileSearchPaneInfo)
         
         self._mgr.AddPane(self.buttonPanel, aui.AuiPaneInfo().
                           Name("button").Caption("Button")
-                          .Layer(1).Bottom().CaptionVisible(False, left=True)
+                          .Layer(0).Bottom().CaptionVisible(False, left=True)
                           .MinimizeButton(True).CloseButton(False))
-
+        
         self._mgr.Update()
+
 
 
 class CreateFileSearchPanel(wx.Panel):
@@ -87,21 +100,21 @@ class CreateFileSearchPanel(wx.Panel):
         containgTextLabel = wx.StaticText(self, label='Containing Text:')
         wildTextLabel = wx.StaticText(self, label='(*= any string, ?= any character, \= escape for literals: *?\)')
 #         self.search = wx.SearchCtrl(self, size=(200,-1), style=wx.TE_PROCESS_ENTER)
-        initialList = []
-        searchTextCb = wx.ComboBox(self, wx.NewIdRef(), "", (90, 50),
-                         (160, -1), initialList,
+        self.initialSearchList = []
+        self.searchTextCb = wx.ComboBox(self, wx.NewIdRef(), "", (90, 50),
+                         (160, -1), self.initialSearchList,
                          wx.CB_DROPDOWN
                          # | wx.TE_PROCESS_ENTER
                          # | wx.CB_SORT
                          )
         
-        caseSensitiveOpt = wx.CheckBox(self, -1, "Case Sensitive")
-        regularExpressionOpt = wx.CheckBox(self, -1, "Regular expression")
-        wholeWordOpt = wx.CheckBox(self, -1, "Whole word")
-        vbox2.AddMany([caseSensitiveOpt, regularExpressionOpt, wholeWordOpt])
+        self.caseSensitiveOpt = wx.CheckBox(self, -1, "Case Sensitive")
+        self.regularExpressionOpt = wx.CheckBox(self, -1, "Regular expression")
+        self.wholeWordOpt = wx.CheckBox(self, -1, "Whole word")
+        vbox2.AddMany([self.caseSensitiveOpt, self.regularExpressionOpt, self.wholeWordOpt])
         
         vbox1.Add(containgTextLabel, 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.Top, 0)
-        vbox1.Add(searchTextCb, 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.Top, 0)
+        vbox1.Add(self.searchTextCb, 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.Top, 0)
         vbox1.Add(wildTextLabel, 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.Top, 0)
 
 #         vbox1.Add(vbox1, 0, wx.EXPAND | wx.ALL, 0)
@@ -112,9 +125,9 @@ class CreateFileSearchPanel(wx.Panel):
         vBoxFileName = wx.BoxSizer(wx.VERTICAL)
         fileNamePatternLabel = wx.StaticText(self, label='File name patterns (separated by comma):')
         wildFileNameTextLabel = wx.StaticText(self, label='(*= any string, ?= any character, \= escape for literals: *?\)')
-        initialList = []
+        self.initialFileNameList = []
         fileNameCb = wx.ComboBox(self, wx.NewIdRef(), "", (90, 50),
-                         (160, -1), initialList,
+                         (160, -1), self.initialFileNameList,
                          wx.CB_DROPDOWN
                          # | wx.TE_PROCESS_ENTER
                          # | wx.CB_SORT
@@ -136,36 +149,36 @@ class CreateFileSearchPanel(wx.Panel):
         ########################## Search in ##########################################
         search_in_title = wx.StaticBox(self, -1, "Search In")
         search_in_box = wx.StaticBoxSizer(search_in_title, wx.HORIZONTAL)
-        cb1 = wx.CheckBox(self, -1, "Derived resources")
-        cb2 = wx.CheckBox(self, -1, "Binary files")
-        search_in_box.AddMany([ cb1, cb2, ])
+        self.derivedResources = wx.CheckBox(self, -1, "Derived resources")
+        self.binaryFiles = wx.CheckBox(self, -1, "Binary files")
+        search_in_box.AddMany([ self.derivedResources, self.binaryFiles, ])
         vBox.Add(search_in_box, 0, wx.EXPAND | wx.ALL, 5)
-        self.Bind(wx.EVT_CHECKBOX, self.EvtCheckBox, cb1)
-        self.Bind(wx.EVT_CHECKBOX, self.EvtCheckBox, cb2)        
+        self.Bind(wx.EVT_CHECKBOX, self.EvtCheckBox, self.derivedResources)
+        self.Bind(wx.EVT_CHECKBOX, self.EvtCheckBox, self.binaryFiles)        
         ########################## Scope ##########################################
         
 #         panel = wx.Panel(self, -1)
         box1_title = wx.StaticBox(self, -1, "Scope")
         scope_box = wx.StaticBoxSizer(box1_title, wx.VERTICAL)
 #         grid1 = wx.FlexGridSizer(cols=3)
-        workspaceRadio = wx.RadioButton(self, -1, " Workspace", style=wx.RB_GROUP)
-        resourceInActiveEditorRadio = wx.RadioButton(self, -1, " Resource in active editor")
-        enclosingProjectRadio = wx.RadioButton(self, -1, "Enclosing project")
-        workingSetRadio = wx.RadioButton(self, -1, "Working set:")
-        wokingSetText = wx.TextCtrl(self, -1, "")
+        self.workspaceRadio = wx.RadioButton(self, -1, " Workspace", style=wx.RB_GROUP)
+        self.resourceInActiveEditorRadio = wx.RadioButton(self, -1, " Resource in active editor")
+        self.enclosingProjectRadio = wx.RadioButton(self, -1, "Enclosing project")
+        self.workingSetRadio = wx.RadioButton(self, -1, "Working set:")
+        self.wokingSetText = wx.TextCtrl(self, -1, "")
 
         chooseBtn = wx.Button(self, 50, "Choose...")
         chooseBtn.SetToolTip("Choose...")
         self.Bind(wx.EVT_BUTTON, self.onChooseBtn, chooseBtn)
         
         hb1 = wx.BoxSizer(wx.HORIZONTAL)
-        hb1.Add(workspaceRadio, 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, 5)
-        hb1.Add(resourceInActiveEditorRadio, 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, 5)
-        hb1.Add(enclosingProjectRadio, 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, 5)
+        hb1.Add(self.workspaceRadio, 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, 5)
+        hb1.Add(self.resourceInActiveEditorRadio, 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, 5)
+        hb1.Add(self.enclosingProjectRadio, 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, 5)
         
         hb2 = wx.BoxSizer(wx.HORIZONTAL)
-        hb2.Add(workingSetRadio, 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, 5)
-        hb2.Add(wokingSetText, 1, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, 5)
+        hb2.Add(self.workingSetRadio, 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, 5)
+        hb2.Add(self.wokingSetText, 1, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, 5)
         hb2.Add(chooseBtn, 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, 5)
         scope_box.Add(hb1, 0, wx.EXPAND | wx.ALL, 5)
         scope_box.Add(hb2, 1, wx.EXPAND | wx.ALL, 5)
@@ -218,7 +231,7 @@ class CreateButtonPanel(wx.Panel):
         hbox.Add(searchBtn)    
         hbox.Add(cancelButton)    
 #         sizer.Add(cancelButton, 0, wx.ALIGN_RIGHT | wx.RIGHT | wx.BOTTOM)
-        sizer.Add(hbox, 0, wx.ALIGN_RIGHT | wx.ALL,10)
+        sizer.Add(hbox, 0, wx.ALIGN_RIGHT | wx.ALL, 10)
 #         sizer.Add(vBox, 1, wx.EXPAND , 0)
         self.SetAutoLayout(True)
         self.SetSizer(sizer)
@@ -228,6 +241,23 @@ class CreateButtonPanel(wx.Panel):
 
     def onSearch(self, event):
         logger.debug('onSearch')
+        fileSearch = self.GetParent()._mgr.GetPaneByName('fileSearch').window
+        logger.debug(f'searchText:{fileSearch.searchTextCb.GetValue()} \ncaseSensitive: {fileSearch.caseSensitiveOpt.GetValue()}')
+        search = {
+            'searchText': fileSearch.searchTextCb.GetValue(),
+            'caseSensitive': fileSearch.caseSensitiveOpt.GetValue(),
+            'regularExpression': fileSearch.regularExpressionOpt.GetValue(),
+            'wholeWord': fileSearch.wholeWordOpt.GetValue(),
+            'workspace': fileSearch.workspaceRadio.GetValue(),
+            'resourceInActiveEditor': fileSearch.resourceInActiveEditorRadio.GetValue(),
+            'enclosingProject': fileSearch.enclosingProjectRadio.GetValue(),
+            'workingSet': fileSearch.workingSetRadio.GetValue(),
+            'wokingSet': fileSearch.wokingSetText.GetValue(),
+            'derivedResources': fileSearch.derivedResources.GetValue(),
+            'binaryFiles': fileSearch.binaryFiles.GetValue(),
+            
+            }
+        logger.debug(fileSearch.searchTextCb.GetValue())
 
     def onCancelButtonClick(self, event):
         logger.debug('onCancelButtonClick')
@@ -238,7 +268,7 @@ class CreateButtonPanel(wx.Panel):
 if __name__ == '__main__':
     app = wx.App(False)
 #     frame = CreateTableFrame(None, 'table creation')
-    frame = SearchPanelsFrame(None, size=(800, 400))
+    frame = SearchPanelsFrame(None, size=(800, 450))
     frame.CenterOnScreen()
 #     frame.setData(tableDict)
 #     frame.Show()
